bea96bbeb0e44ecabb097c7a9a1d5fe55af448a0
Marcus Ziadé <marcus.ziade@ouraring.com>
2023-09-03 09:41:01 +0300

    Improve main.go readability

--- main.go
+++ main.go
@@ -10,40 +10,59 @@ import (
 	"github.com/marcusziade/github-api/endpoints"
 )
 
+const GitHubTokenEnv = "GITHUB_TOKEN"
+const DefaultPages = 2
+const MaxPages = 5
+
 func main() {
-	token := os.Getenv("GITHUB_TOKEN")
+	token := os.Getenv(GitHubTokenEnv)
 	if token == "" {
-		log.Fatal("GitHub token must be set in environment variable GITHUB_TOKEN")
+		log.Fatal("GitHub token missing.")
 	}
 
 	e := endpoints.NewEndpoints(&http.Client{})
 
-	http.HandleFunc("/github_user/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/github_user/", getUserHandler(e, token))
+	http.HandleFunc("/user", getAuthUserHandler(e, token))
+	http.HandleFunc("/github_starred/", getStarredHandler(e, token))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getUserHandler(e *endpoints.Endpoints, token string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		username := r.URL.Path[len("/github_user/"):]
 		user, err := e.GetUser(username, token)
 		handleRequest(w, user, err)
-	})
+	}
+}
 
-	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+func getAuthUserHandler(e *endpoints.Endpoints, token string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		user, err := e.GetAuthenticatedUser(token)
 		handleRequest(w, user, err)
-	})
+	}
+}
 
-	http.HandleFunc("/github_starred/", func(w http.ResponseWriter, r *http.Request) {
-		pagesStr := r.URL.Query().Get("pages")
-		pages, err := strconv.Atoi(pagesStr)
-		if err != nil || pages <= 0 {
-			pages = 2
-		}
-		if pages > 5 {
-			pages = 5
-		}
+func getStarredHandler(e *endpoints.Endpoints, token string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		pages := getPageCount(r)
 		username := r.URL.Path[len("/github_starred/"):]
 		starredRepos, err := e.GetStarredRepos(username, token, pages)
 		handleRequest(w, starredRepos, err)
-	})
+	}
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func getPageCount(r *http.Request) int {
+	pagesStr := r.URL.Query().Get("pages")
+	pages, err := strconv.Atoi(pagesStr)
+	if err != nil || pages <= 0 {
+		return DefaultPages
+	}
+	if pages > MaxPages {
+		return MaxPages
+	}
+	return pages
 }
 
 func handleRequest(w http.ResponseWriter, data interface{}, err error) {
@@ -54,7 +73,7 @@ func handleRequest(w http.ResponseWriter, data interface{}, err error) {
 
 	prettyJSON, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Internal error", http.StatusInternalServerError)
 		return
 	}
 
